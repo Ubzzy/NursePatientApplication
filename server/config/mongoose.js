fabeb0c2e98a834017771daf541e5761105acf07
Ubaid Delawala <delawalaubaid@gmail.com>
2022-12-14 15:59:54 -0500

    set db connection to cloud

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,8 +3,7 @@ let mongoose = require("mongoose");
 module.exports = function () {
     // Set up MongoDb Connection
     let db = mongoose
-        // .connect("mongodb+srv://Kunj:KunjCollege@cluster0.k3oag.mongodb.net/graphql-db", {
-        .connect("mongodb://127.0.0.1/group4Project", {
+        .connect("mongodb+srv://Kunj:KunjCollege@cluster0.k3oag.mongodb.net/graphql-db", {
             useNewUrlParser: true,
         })
         .then(() => console.log("MongoDB Connected Successfully!"))
